c7e4b20dc0c15186650abd87299b45b159270e84
Jim Danz <danz@fas.harvard.edu>
2012-02-02 13:36:57 -0500

    Added submit script (wrapper around git commands)

---
+++ submit.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+die () {
+	echo "$0: error: $*" >&2
+	exit 1
+}
+
+echo "CS51: Submitting Problem Set $1"
+
+git commit -m"Done with ps$1.  Ready to submit." || die "commit failed"
+git tag -f ps$1-submission || die "tagging failed"
+git push --tags || die "pushing failed"
+
+echo "Submission complete."
+echo "You should now check that you have received a confirmation email."
